4619abd5b0ecf8c0a5dd0602a978734199e36751
Roma Ag <93056140+Tiraniell@users.noreply.github.com>
2022-03-17 21:23:46 +0300

    new script
    C# scripts "Jump"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,50 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 2f;
+    [SerializeField] float jumpForce = 2f;
+
+    private readonly Vector3 jumpDirection = Vector3.up;
+    public bool isGround { get; private set; }
+    private new Rigidbody rigidbody;
+
+
+
+    private void Start()
+    {
+        this.rigidbody = this.GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         GetInput();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    
+    }
+    void Jump()
+    { if (this.isGround)
+            this.rigidbody.AddForce(this.jumpDirection * this.jumpForce, ForceMode.Impulse);
+                  
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        var ground = other.gameObject.GetComponentInParent<ground>();
+        if (ground)
+            this.isGround = true;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        var ground = other.gameObject.GetComponentInParent<ground>();
+        if (ground)
+            this.isGround = false;
     }
 
     private void GetInput()
